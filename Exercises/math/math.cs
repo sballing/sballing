0a34b54696874f657892f7da0bc0cf3fa928a646
sballing <soerenballing@gmail.com>
2022-02-08 11:27:44 +0100

    Added first draft of math exercise

---
+++ Exercises/math/math.cs
@@ -0,0 +1,9 @@
+class math{
+	static void Main() {
+		double sqrt2 = System.Math.Sqrt(2.0);
+		System.Console.Write($"sqrt(2) = {sqrt2}\n");
+		System.Console.Write($"sqrt2*sqrt2 = {sqrt2*sqrt2} (should be equal 2)\n");
+	}
+
+
+}
