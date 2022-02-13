af3b4095354d8b3152e294333dda1d1efd672e94
sballing <soerenballing@gmail.com>
2022-02-13 09:47:30 +0100

    Uploading final version of the vec homework

--- Homework/vec/main.cs
+++ Homework/vec/main.cs
@@ -1,11 +1,63 @@
 using static vec;
 public class main{
 	static void Main() {
-		vec u = new vec();
-		vec v = new vec(1,1,2);
+		// Iniialize a null vector and two non-zero vector
+		vec n = new vec();
+		vec u = new vec(2,1,3);
+		vec v = new vec(4,1,5);
+
+		// Check the print function
+		System.Console.WriteLine("Print the 3 three initialized vectors");
+		n.print("n = ");
 		u.print("u = ");	
 		v.print("v = ");
-		u.print();
-		v.print();
+
+		// Check the mathematical operators
+		System.Console.WriteLine("");
+		System.Console.WriteLine("Testing the basic mathematical operators");
+		(u+v).print($"u + v = ");
+		(u-v).print($"u - v = ");
+		(-u).print($"-u = ");
+		(2.5*u).print($"2.5 * u = ");
+		vec x = (u*2.5);
+		x.print($"u * 2.5 = "); // For some reason, this cannot be put into one statement?
+
+		// Check the vector operators
+		System.Console.WriteLine("");
+		System.Console.WriteLine("Testing the vector operators");
+		System.Console.WriteLine($"First dot method: u*v = {u.dot(v)}");
+		System.Console.WriteLine($"Second dot method: u*v = {dot(u,v)}");
+		System.Console.Write($"First cross-product method: "); u.cross(v).print("u x v = ");
+		System.Console.Write($"Second cross-product method: "); cross(u,v).print("u x v = ");
+		System.Console.WriteLine($"Norm |u| = {u.norm()}");
+		System.Console.WriteLine($"Overriden ToString method for vector u: {u.ToString()}, standard just states \"vec\"");
+
+		// Test the approx method
+		vec a = new vec();
+		vec b = new vec();
+		vec c = new vec(1,1,1);
+		vec d = new vec(1,1,1);
+		vec e = new vec(1+1e-10, 1+1e-10, 1+1e-10);
+		vec f = new vec(1+1e-5, 1+1e-5, 1+1e-5);
+		vec g = new vec(1+1e-9, 1+1e-9, 1+1e-9);
+		vec h = new vec(1+1e-8, 1+1e-8, 1+1e-8);
+		vec i = new vec(1-1e-10, 1-1e-10, 1-1e-10);
+		vec j = new vec(1-1e-5, 1-1e-5, 1-1e-5);
+		vec k = new vec(1-1e-9, 1-1e-9, 1-1e-9);
+		vec l = new vec(1-1e-8, 1-1e-8, 1-1e-8);
+		System.Console.WriteLine("");
+		System.Console.WriteLine("Testing the approx method");
+		System.Console.WriteLine($"{a.ToString()} == {b.ToString()}: {approx(a,b)}");
+		System.Console.WriteLine($"{a.ToString()} == {c.ToString()}: {approx(a,c)}");
+		System.Console.WriteLine($"{c.ToString()} == {d.ToString()}: {approx(c,d)}");
+		System.Console.WriteLine($"{c.ToString()} == {e.ToString()}: {approx(c,e)}");
+		System.Console.WriteLine($"{c.ToString()} == {f.ToString()}: {approx(c,f)}");
+		System.Console.WriteLine($"{c.ToString()} == {g.ToString()}: {approx(c,g)}");
+		System.Console.WriteLine($"{c.ToString()} == {h.ToString()}: {approx(c,h)}");
+		System.Console.WriteLine($"{c.ToString()} == {i.ToString()}: {approx(c,i)}");
+		System.Console.WriteLine($"{c.ToString()} == {j.ToString()}: {approx(c,j)}");
+		System.Console.WriteLine($"{c.ToString()} == {k.ToString()}: {approx(c,k)}");
+		System.Console.WriteLine($"{c.ToString()} == {l.ToString()}: {approx(c,l)}");
+		System.Console.WriteLine($"The result of all tests fits with the criteria of the approx method!");
 	}
 }
--- Homework/vec/vec.cs
+++ Homework/vec/vec.cs
@@ -1,5 +1,6 @@
 using System;
 using static System.Console;
+using static System.Math;
 public class vec{
 	public double x,y,z; // 3 entries of the vector
 
@@ -18,7 +19,61 @@ public class vec{
 		WriteLine($"{s}({x}, {y}, {z})");
 	}
 	public void print() {
-		print("");
+		this.print("");
+	}
+
+	// Implementing mathematical operators
+	public static vec operator*(vec v, double c) {
+		return new vec(c*v.x, c*v.y, c*v.z);
+	}
+	public static vec operator*(double c, vec v) {
+		return v*c;
+	}
+	public static vec operator+(vec u, vec v) {
+		return new vec(u.x+v.x, u.y+v.y, u.z+v.z);
+	}
+	public static vec operator-(vec u, vec v) {
+		return new vec(u.x-v.x, u.y-v.y, u.z-v.z);
+	}
+	public static vec operator-(vec v) {
+		return new vec(-v.x, -v.y, -v.z);
+	}
+
+	// Implementing dot-product, cross-product, norm and overriding ToString
+	public double dot(vec other) {
+		return this.x*other.x + this.y*other.y + this.z*other.z;
+	}
+	public static double dot(vec u, vec v) {
+		return u.x*v.x + u.y*v.y + u.z*v.z;
+	}
+	public vec cross(vec other) {
+		return new vec(this.y*other.z-this.z*other.y, this.z*other.x-this.x*other.z, this.x*other.y-this.y*other.x);
+	}
+	public static vec cross(vec u, vec v) {
+		return new vec(u.y*v.z-u.z*v.y, u.z*v.x-u.x*v.z, u.x*v.y-u.y*v.x);
+	} 
+	public double norm() {
+		return Sqrt(this.x*this.x + this.y*this.y + this.z*this.z);
+	}
+	public override string ToString() {
+		return $"vec[({x}, {y}, {z})]";
+	}
+
+	// Implementing the approx method
+	public bool approx(vec other) {
+		double tau = 1e-9; double eps = 1e-9;
+		if (Abs(this.x-other.x) < tau && Abs(this.y-other.y) < tau && Abs(this.z-other.z) < tau) {
+			return true;
+		}
+		if (Abs(this.x-other.x)/(Abs(this.x)+Abs(other.x)) < eps && Abs(this.y-other.y)/(Abs(this.y)+Abs(other.y)) < eps && Abs(this.z-other.z)/(Abs(this.z)+Abs(other.z)) < eps) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	public static bool approx(vec u, vec v) {
+		return u.approx(v);
 	}
 
 }
