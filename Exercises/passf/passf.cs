07be12fdf17b995542cd2cf7cdf2f106f72e5029
sballing <soerenballing@gmail.com>
2022-02-18 14:28:02 +0100

    Adjusting the passf exercise

--- Exercises/passf/passf.cs
+++ Exercises/passf/passf.cs
@@ -4,15 +4,17 @@ using static System.Math;
 public class passf{
 
 	public static void Main() {
-		int k = 1;
-		WriteLine($"Making table for sin({k}x)");
-		make_table(x => Sin(x), 0, 2*PI, 0.1);
+		for(int k = 1; k <= 3; k++){
+			WriteLine("");
+			WriteLine($"Making table for sin({k}x)");
+			make_table(x => Sin(k*x), 0, 2*PI, 0.1);
+		}
 	}
 
 	private static void make_table(Func<double,double> f, double a, double b, double dx) {
 		double i = a;
 		while (i < b) {
-			WriteLine($"{f(i)}");
+			WriteLine($"x = {i}, sin(x) = {f(i)}");
 			i = i + dx;
 		} 
 	}
