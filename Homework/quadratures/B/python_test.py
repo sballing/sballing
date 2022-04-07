e547d319e88db819b2dd805c4e23c667bb585226
sballing <soerenballing@gmail.com>
2022-04-07 11:46:53 +0200

    Fixing quadratures B

--- Homework/quadratures/B/python_test.py
+++ Homework/quadratures/B/python_test.py
@@ -2,19 +2,14 @@ import numpy as np
 from scipy import integrate
 
 def f1(x):
-    global i
-    i = i+1
     return 1/np.sqrt(x)
 
 def f2(x):
-    global j
-    j = j+1
     return np.log(x)/np.sqrt(x)
 
-i = 0
-j = 0
-res1 = integrate.quad(f1, 0, 1, epsabs=0.001, epsrel=0.001)
-res2 = integrate.quad(f2, 0, 1, epsabs=0.001, epsrel=0.001)
 
-print("1/sqrt(x) - Python implementation: " + str(res1[0]) + ". It took " + str(i) + " iterations.")
-print("log(x)/sqrt(x) - Python implementation: " + str(res2[0]) + ". It took " + str(j) + " iterations.")
+res1, err1, infodict1 = integrate.quad(f1, 0, 1, epsabs=0.001, epsrel=0.001, full_output=True)
+res2, err2, infodict2 = integrate.quad(f2, 0, 1, epsabs=0.001, epsrel=0.001, full_output=True)
+
+print("1/sqrt(x) - Python implementation: " + str(res1) + ". It took " + str(infodict1['neval']) + " iterations.")
+print("log(x)/sqrt(x) - Python implementation: " + str(res2) + ". It took " + str(infodict2['neval']) + " iterations.")
