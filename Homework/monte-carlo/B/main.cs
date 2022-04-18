4e51b0565355f0baf98c101f1563cb698c9b7550
sballing <soerenballing@gmail.com>
2022-04-18 11:30:36 +0200

    Uploading Monte-Carlo B

---
+++ Homework/monte-carlo/B/main.cs
@@ -0,0 +1,44 @@
+using System;
+using static System.Console;
+using static System.Math;
+public class main{
+
+	public static void Main(){
+		WriteLine("----------------------------------------------------");
+		WriteLine("Testing both integrator routines on some interesting integrals\n");
+
+
+		WriteLine("Integrating x*y dx dy, x = 0..1, y = 0..1");
+		Func<vector,double> f1 = x => x[0]*x[1];
+		vector a1 = new double[2] {0,0};
+		vector b1 = new double[2] {1,1};
+		var res1_plain = mcint.plainmc(f1, a1, b1, 50000);
+		var res1_halton = mcint.haltonmc(f1, a1, b1, 50000);
+		WriteLine($"\tPlain routine gives: {res1_plain.Item1} with error {res1_plain.Item2}");
+		WriteLine($"\tHalton routine gives: {res1_halton.Item1} with error {res1_halton.Item2}");
+		WriteLine("\tResult should be 0.25");
+
+		WriteLine("\nIntegrating (x + sin(y) + 1) dx dy, x = 0..2, y = -pi..pi");
+		Func<vector,double> f2 = x => x[0] + Sin(x[1]) + 1;
+		vector a2 = new double[2] {0,-PI};
+		vector b2 = new double[2] {2,PI};
+		var res2_plain = mcint.plainmc(f2, a2, b2, 50000);
+		var res2_halton = mcint.haltonmc(f2, a2, b2, 50000);
+		WriteLine($"\tPlain routine gives: {res2_plain.Item1} with error {res2_plain.Item2}");
+		WriteLine($"\tHalton routine gives: {res2_halton.Item1} with error {res2_halton.Item2}");
+		WriteLine("\tResult should be 8*pi = 25.133");
+
+		WriteLine("\nIntegrating 1/([1 - cos(x)*cos(y)*cos(z)]*pi^3) dx dy dz, x = 0..pi, y = 0..pi, z = 0..pi");
+		Func<vector,double> f3 = x => 1/((1 - Cos(x[0])*Cos(x[1])*Cos(x[2]))*Pow(PI,3));
+		vector a3 = new double[3] {0,0,0};
+		vector b3 = new double[3] {PI, PI, PI};
+		var res3_plain = mcint.plainmc(f3, a3, b3, 10000000);
+		var res3_halton = mcint.haltonmc(f3, a3, b3, 10000000);
+		WriteLine($"\tPlain routine gives: {res3_plain.Item1} with error {res3_plain.Item2}");
+		WriteLine($"\tHalton routine gives: {res3_halton.Item1} with error {res3_halton.Item2}");
+		WriteLine("\tResult should be 1.393203");
+		WriteLine("----------------------------------------------------");
+
+	}
+
+}
---
+++ Homework/monte-carlo/B/mcint.cs
@@ -0,0 +1,115 @@
+using System;
+using static System.Math;
+public class mcint{
+
+	public static (double, double) plainmc(Func<vector,double> f, vector a, vector b, int N){
+		var rand = new Random();
+
+		int dim = a.size;
+		double V = 1.0; 
+		for(int i=0; i<dim; i++){
+			V *= b[i] - a[i];
+		}
+		double sum = 0, sum2 = 0;
+
+		var x = new vector(dim);
+		for(int i=0; i<N; i++){
+			for(int k=0; k<dim; k++){
+				x[k] = a[k] + rand.NextDouble()*(b[k] - a[k]);
+			}
+			double fx = f(x);
+			sum += fx;
+			sum2 += fx*fx;
+		}
+
+		double mean = sum/N;
+		double sigma = Sqrt(sum2/N - mean*mean);
+		var res = (mean*V, sigma*V/Sqrt(N));
+		return res;
+	}
+
+	public static (double, double) haltonmc(Func<vector,double> f, vector a, vector b, int N){
+		int dim = a.size;
+		double V = 1.0;
+		for(int i=0; i<dim; i++){
+			V *= b[i] - a[i];
+		}
+		double sum = 0, sum2 = 0; // Two sequences are used in order to estimate the error
+
+		double[] x = new double[dim];
+		double[] x2 = new double[dim];
+
+        int offset=0; // We can skip diverging points
+
+
+		for(int i=0; i<N; i++){
+			double[] quas = halton(i+offset, dim);
+			double[] quas2 = halton(i+offset, dim, 4); //Base shifted to 2nd sequence
+			
+			for(int k=0; k<dim; k++){
+				x[k] = a[k] + quas[k]*(b[k] - a[k]);
+				x2[k] = a[k] + quas2[k]*(b[k] - a[k]);
+			}
+
+			double fx = f(x);
+			double fx2 = f(x2);
+
+			if (double.IsNaN(fx) || double.IsInfinity(fx) || double.IsNaN(fx2) || double.IsInfinity(fx2)){
+				--i; ++offset;
+			}
+
+			else{
+				sum += fx;
+				sum2 += fx2;
+			}
+		}
+
+		double mean = sum/N;
+		double mean2 = sum2/N;
+		var res = (mean*V, Abs(mean*V - mean2*V));
+
+		return res;
+
+	}
+
+
+	/*public static double corput(int n, int Base){
+		double q = 0, bk = 1.0/Base;
+		while(n>0){
+			q += (n % Base)*bk;
+			n /= Base;
+			bk /= Base;
+		}
+
+		return q;
+	}*/
+
+	    static double corput(int n , int Base )
+    {
+        double q=0 , bk= 1.0/Base;
+
+        while ( n>0)
+        {
+            q += (n%Base)*bk ;
+            n /= Base;
+            bk /= Base;
+        }
+
+        return q;
+    }
+
+	public static double[] halton(int n, int d, int sec=0){
+		int[] Base = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67};
+		if(d>Base.Length){
+			throw new System.Exception("Dimension is too large for the Halton approach");
+		}
+		
+		double[] x = new double[d];
+
+		for(int i=0; i<d; i++){
+			x[i] = corput(n, Base[(i+sec)%Base.Length]);
+		}
+		return x;
+	} 
+
+}
