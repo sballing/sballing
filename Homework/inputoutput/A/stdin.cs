a276da4d8327314ab3bec64bfcba61c770ccc060
sballing <soerenballing@gmail.com>
2022-05-23 12:38:45 +0200

    Updating inputoutput homework

--- Homework/inputoutput/A/stdin.cs
+++ Homework/inputoutput/A/stdin.cs
@@ -5,17 +5,18 @@ using static System.Math;
 public class stdin {
 	
 	public static void Main() {
-		WriteLine("");
-		WriteLine("Reading from standard input stream:");
+		WriteLine("-------------------------------------------------");
+		WriteLine("Reading from standard input stream and printing their cosine and sine:\n");
+		WriteLine("x\tsin(x)\t\t\tcos(x)\n");
 		char[] delimiters = {' ','\t','\n'};
 		var options = StringSplitOptions.RemoveEmptyEntries;
 		for(string line = ReadLine(); line != null; line = ReadLine()){
 			var words = line.Split(delimiters,options);
 			foreach(var word in words){
 				double x = double.Parse(word);
-				WriteLine($"{x} {Sin(x)} {Cos(x)}");
+				WriteLine($"{x}\t{Sin(x)}\t{Cos(x)}");
             }
         }
-        WriteLine("");
+		WriteLine("-------------------------------------------------");
 	}
 }
