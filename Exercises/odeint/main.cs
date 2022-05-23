8bf8ced2acd68d05e6d43dbf203ff93e08f0ccba
sballing <soerenballing@gmail.com>
2022-05-23 12:02:51 +0200

    Updating odeint exercise

--- Exercises/odeint/main.cs
+++ Exercises/odeint/main.cs
@@ -4,11 +4,17 @@ using static System.Console;
 using static System.Math;
 public static class odeint{
 	public static void Main(){
+		WriteLine("---------------------------------------------------");
+		WriteLine("Using the ode.ivp routine from matlib");
+		WriteLine("to solve the coupled differential equations");
+		WriteLine("from the scipy.integrate.odeint example\n");
+
 		double b=0.25, c=5.0;
 		Func<double,vector,vector> f = delegate(double t, vector y){
 			double theta=y[0], omega=y[1];
 			return new vector(omega, -b*omega-c*Sin(theta));
 		};
+
 		double start=0, stop=10;
 		vector y0 = new vector(PI-0.1, 0.0);
 		var xlist = new List<double>();
@@ -16,8 +22,14 @@ public static class odeint{
 
 		vector ystop = ode.ivp(f, start, y0, stop, xlist, ylist);
 
-		for (int i=0; i<xlist.Count;i++){
-			WriteLine($"{xlist[i]} {ylist[i][0]} {ylist[i][1]}");
+		using(var outfile = new System.IO.StreamWriter("solution.txt")){
+			for (int i=0; i<xlist.Count;i++){
+				outfile.WriteLine($"{xlist[i]} {ylist[i][0]} {ylist[i][1]}");
+			}
 		}
+
+		WriteLine("The solution appears to agree very well");
+		WriteLine("with that given by scipy.integrate.odeint");
+		WriteLine("---------------------------------------------------");
 	}
 }
