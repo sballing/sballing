6a41fe0fff743b0ce7e775dbdf41a576919df952
sballing <soerenballing@gmail.com>
2022-05-23 13:24:23 +0200

    Updating genericlist homework

--- Homework/genericlist/C/main.cs
+++ Homework/genericlist/C/main.cs
@@ -2,7 +2,10 @@ using System;
 using static System.Console;
 public class main{
         public static void Main(){
+                WriteLine("---------------------------------------------------");
                 WriteLine("Demonstating the functionality of the \"chain of node\" implementation:");
+                WriteLine("Three integers (1,2,3) are added to the list.");
+                WriteLine("Next, they are printed again.\n");
                 list<int> a = new list<int>();
                 a.push(1);
                 a.push(2);
@@ -10,5 +13,6 @@ public class main{
                 for(a.start(); a.current != null; a.next()){
                         WriteLine(a.current.item);
                 } 
+                WriteLine("---------------------------------------------------");
         } 
 }
