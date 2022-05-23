9c51f64bde93bbffc429e0a3b8f0285e26bcc99f
sballing <soerenballing@gmail.com>
2022-05-23 11:25:11 +0200

    Updating passfunctions exercise

--- Exercises/passfunctions/passf.cs
+++ Exercises/passfunctions/passf.cs
@@ -3,10 +3,8 @@ using static System.Console;
 
 public class passf{
 	public static void make_table(Func<double,double> f, double a, double b, double dx) {
-		while (a <= b) {
-			WriteLine($"{a}, {f(a)}");
-			a += dx;
-		} 
+		for(double x=a; x<=b; x+=dx){
+			WriteLine($"{x} {f(x)}");
+		}
 	}
-
 }
