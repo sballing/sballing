cab76a82c2d5c7298919144f319207440d542f33
sballing <soerenballing@gmail.com>
2022-05-24 09:39:47 +0200

    Updating minimization homework

--- Homework/minimization/A/main.cs
+++ Homework/minimization/A/main.cs
@@ -4,7 +4,7 @@ using static System.Math;
 public class main{
 
 	public static void Main(){
-		WriteLine("---------------------------------------------------");
+		WriteLine("---------------------------------------------------------------------------------------");
 		WriteLine("Finding minimum of Rosenbrock's valley function:\n");
 
 		Func<vector,double> f1 = x => (1 - x[0])*(1 - x[0]) + 100*(x[1] - x[0]*x[0])*(x[1] - x[0]*x[0]);
@@ -15,8 +15,8 @@ public class main{
 		var (x1,nsteps1) = mini.qnewton(f1, x10, acc: acc1);
 		WriteLine("The minimum was determined to be\n");
 		WriteLine($"\t({x1[0]},{x1[1]}) in {nsteps1} steps. \n\tHere f(x,y) = {f1(x1)}");
-		WriteLine("\n\tOne minimum is at (1,1), where f(x,y) = 0");
-		WriteLine("\n---------------------------------------------------");
+		WriteLine("\n\tFrom Wikipedia:\n\tOne minimum is at (1,1), where f(x,y) = 0");
+		WriteLine("\n---------------------------------------------------------------------------------------");
 
 
 		WriteLine("Finding a minimum of Himmelblau's function:\n");
@@ -28,8 +28,8 @@ public class main{
 		var (x2,nsteps2) = mini.qnewton(f2, x20, acc: acc2);
 		WriteLine("The minimum was determined to be\n");
 		WriteLine($"\t({x2[0]},{x2[1]}) in {nsteps2} steps. \n\tHere f(x,y) = {f2(x2)}");
-		WriteLine("\n\tOne minimum is at (3,2), where f(x,y) = 0");
-		WriteLine("\n---------------------------------------------------");
+		WriteLine("\n\tFrom Wikipedia:\n\tOne minimum is at (3,2), where f(x,y) = 0");
+		WriteLine("\n---------------------------------------------------------------------------------------");
 	}
 
 }
