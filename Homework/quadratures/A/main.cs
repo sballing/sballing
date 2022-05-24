e2de730a8aea4ac44a6d6d3e09f85f8d8dc3fc1f
sballing <soerenballing@gmail.com>
2022-05-24 09:02:17 +0200

    Updating quadratures homework

--- Homework/quadratures/A/main.cs
+++ Homework/quadratures/A/main.cs
@@ -9,7 +9,7 @@ public class main{
 	}
 
 	public static void Main(){
-		WriteLine("------------------------------------");
+		WriteLine("-------------------------------------------------------");
 		WriteLine("Testing implementation on some interesting integrals:\n");
 
 		double a=0.0, b=1.0;
@@ -23,15 +23,15 @@ public class main{
 		double res2 = integrator.quad(f2, a, b);
 		double res3 = integrator.quad(f3, a, b);
 		double res4 = integrator.quad(f4, a, b);
-		WriteLine($"Int sqrt(x) from 0 to 1: {res1}");
-		WriteLine($"Int 1/sqrt(x) from 0 to 1: {res2}");
-		WriteLine($"Int 4*sqrt(1-x^2) from 0 to 1: {res3}");
-		WriteLine($"Int log(x)/sqrt(x) from 0 to 1: {res4}");
-		WriteLine("\n------------------------------------");
+		WriteLine($"Int sqrt(x) from 0 to 1: {res1} (should be 2/3 = 0.666...)");
+		WriteLine($"Int 1/sqrt(x) from 0 to 1: {res2} (should be 2)");
+		WriteLine($"Int 4*sqrt(1-x^2) from 0 to 1: {res3} (should be pi = 3.14159...)");
+		WriteLine($"Int log(x)/sqrt(x) from 0 to 1: {res4} (should be -4)");
+		WriteLine("\n-------------------------------------------------------");
 
 		WriteLine("Using the implemented integrator to estimate the error function");
-		WriteLine("The result is plotted in gamma.png along some table values");
-		WriteLine("------------------------------------");
+		WriteLine("The result is plotted in \'gamma.png\' along some table values");
+		WriteLine("-------------------------------------------------------");
 
 		using(var outfile = new System.IO.StreamWriter("gamma.txt")){
 			for(double x=-3; x<=3; x+=1.0/16){
