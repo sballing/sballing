1645598a90f3b740c52e0ce31b0696d2610e3ee7
sballing <soerenballing@gmail.com>
2022-06-13 16:07:46 +0200

    Exam commit, not quite finished yet

--- Exam/berrut.cs
+++ Exam/berrut.cs
@@ -2,16 +2,11 @@ using System;
 using static System.Math;
 public class berrut{
 
-	public static double B1_interp(double x, double[] xs, double[] ys){ // x: point to evaluate, xs, ys: data data points to interpolate
+	/*public static double B1_interp(double x, double[] xs, double[] ys){ // x: point to evaluate, xs, ys: data data points to interpolate
 		int n = xs.Length;
 
 		double numer = 0;
 		double denom = 0;
-
-		/*for(int i=0; i<n; i++){
-			numer += Pow(-1,i)/(x - xs[i])*ys[i];
-			denom += Pow(-1,i)/(x - xs[i]);
-		}*/
 
 		for(int i=0; i<n; i++){
 			double xdiff = x - xs[i];
@@ -21,10 +16,90 @@ public class berrut{
 		}
 
 		return numer/denom;
+	}*/
+
+	public static double[] linspace(double x0, double x1, int N){
+		double step = (x1 - x0)/(N - 1);
+		double[] y = new double[N];
+		for(int i=0; i<N; i++){
+			y[i] = x0 + step*i;
+		}
+		return y;
 	}
 
-	public static void test(){
-		System.Console.WriteLine("HelloWorld2");
+	public static (double[],double[]) B1_interp(double[] xs, double[] ys, double x0, double x1, int N){
+		int n = xs.Length;
+
+		double[] c = new double[n+1];
+		c[0] = 1.0/2;
+		c[n] = 1.0/2*Pow(-1,n);
+
+		for(int i=1; i<n; i++){
+			c[i] = Pow(-1,i);
+		}
+
+		double[] xx = linspace(x0, x1, N);
+
+		double[] numer = new double[xx.Length];
+		double[] denom = new double[xx.Length];
+		double[] res = new double[xx.Length];
+
+		for(int i=0; i<xx.Length; i++){
+			for(int j=0; j<n; j++){
+				double xdiff = xx[i] - xs[j];
+				double temp = c[j]/xdiff;
+				numer[i] += temp*ys[j];
+				denom[i] += temp;
+			}
+			res[i] = numer[i]/denom[i];
+		}
+
+		/*for(int j=0; j<n; j++){
+			for(int i=0; i<xx.Length; i++){
+				double xdiff = xx[i]-xs[j];
+				double temp = c[j]/xdiff;
+				numer[i] = numer[i] + temp*ys[j];
+				denom[i] = denom[i] + temp;
+				res[i] = numer[i]/denom[i];
+			}
+		}*/
+
+		return (xx, res);
 	}
+
+	/*public static (double[],double[]) B1_interp(double[] xs, double[] ys, double x0, double x1, int N){
+		int n = xs.Length;
+
+		double[] c = new double[n+1];
+		for(int i=0; i<=n; i++){
+			if(i==0 || i==n+1){
+				c[i] = 1.0/2*Pow(-1,i);
+			}
+			else{
+				c[i] = Pow(-1,i);
+			}
+		}
+
+		double[] xx = linspace(x0, x1, N);
+
+		double[] numer = new double[xx.Length];
+		double[] denom = new double[xx.Length];
+
+		for(int j=0; j<n; j++){
+			for(int i=0; i<xx.Length; i++){
+				double xdiff = xx[i]-xs[j];
+				double temp = c[j]/xdiff;
+				numer[i] += temp*ys[j];
+				denom[i] += temp;
+			}
+		}
+
+		double[] res = new double[numer.Length];
+		for(int i=0; i<res.Length; i++){
+			res[i] = numer[i]/denom[i];
+		}
+
+		return (xx, res);
+	}*/
 
 }
--- Exam/main.cs
+++ Exam/main.cs
@@ -3,16 +3,31 @@ using static System.Console;
 using static System.Math;
 public class main{
 
+	public static double[] linspace(double x0, double x1, int N){
+		double step = (x1 - x0)/(N - 1);
+		double[] y = new double[N];
+		for(int i=0; i<N; i++){
+			y[i] = x0 + step*i;
+		}
+		return y;
+	}
+
+
 	public static void Main(){
 		WriteLine("For details about the examination project, see the ReadMe-file");
 
 		// Test 1 - setting data points
-		//double[] xs_1 = new double[] {-3.5, -2.5, -1.5, -0.5, 0.5, 1.5, 2.5, 3.5};
 		double[] xs_1 = new double[] {-1.05, -0.75, -0.45, -0.15, 0.15, 0.45, 0.75, 1.05};
 		double[] ys_1 = new double[] {0, 0, 0, 0, 1, 1, 1, 1};
 
 		// Test 2 - setting data points
-		double[] xs_2 = new double[] {-1, -0.75, -0.50, -0.25, 0, 0.25, 0.50, 0.75, 1};
+		int n = 12;
+		/*double[] xs_2 = new double[n];
+		for(int i=0; i<xs_2.Length; i++){
+			xs_2[i] = Cos(PI*(i-1)/n);
+		}*/
+		double[] xs_2 = linspace(-1, 1, n);
+		//double[] xs_2 = new double[] {-1, -0.75, -0.50, -0.25, 0, 0.25, 0.50, 0.75, 1};
 		double[] ys_2 = new double[xs_2.Length];
 		Func<double,double> f2 = x => 1/(1 + 25*x*x);
 		for(int i=0; i<xs_2.Length; i++){
@@ -20,7 +35,12 @@ public class main{
 		}
 
 		// Test 3 - setting data points
-		double[] xs_3 = new double[] {-1, -0.75, -0.50, -0.25, 0, 0.25, 0.50, 0.75, 1};
+		/*double[] xs_3 = new double[n];
+		for(int i=0; i<xs_3.Length; i++){
+			xs_3[i] = Cos(PI*(i-1)/n);
+		}*/
+		double[] xs_3 = linspace(-1, 1, n);
+		//double[] xs_3 = new double[] {-1, -0.75, -0.50, -0.25, 0, 0.25, 0.50, 0.75, 1};
 		double[] ys_3 = new double[xs_3.Length];
 		Func<double,double> f3 = x => Abs(x) + x/2 - x*x;
 		for(int i=0; i<xs_3.Length; i++){
@@ -28,8 +48,13 @@ public class main{
 		}
 
 		// Test 4 - setting data points
-		double[] xs_4 = new double[] {-1, -0.75, -0.50, -0.25, 0, 0.25, 0.50, 0.75, 1};
-		double[] ys_4 = new double[xs_3.Length];
+		/*double[] xs_4 = new double[n];
+		for(int i=0; i<xs_4.Length; i++){
+			xs_4[i] = Cos(PI*i/n);
+		}*/
+		double[] xs_4 = linspace(-1, 1, n);
+		//double[] xs_4 = new double[] {-1, -0.75, -0.50, -0.25, 0, 0.25, 0.50, 0.75, 1};
+		double[] ys_4 = new double[xs_4.Length];
 		Func<double,double> f4 = x => Cos(10*x)*Exp(-x);
 		for(int i=0; i<xs_4.Length; i++){
 			ys_4[i] = f4(xs_4[i]);
@@ -37,6 +62,61 @@ public class main{
 
 		// Testing the Berrut interpolation algorithm
 		using(var outfile = new System.IO.StreamWriter("berrut_interpolation.txt")){
+
+			//2nd approach
+			// Test 1 - interpolating the function
+			for(int i=0; i<xs_1.Length; i++){
+				outfile.WriteLine($"{xs_1[i]} {ys_1[i]}");
+			}
+
+			outfile.WriteLine("\n");
+
+			var (xx1, res1) = berrut.B1_interp(xs_1, ys_1, -1.051, 1.051, 1000);
+			for(int i=0; i<res1.Length; i++){
+				outfile.WriteLine($"{xx1[i]} {res1[i]}");
+			}
+
+			// Test 2 - interpolating the function
+			outfile.WriteLine("\n");
+			for(int i=0; i<xs_2.Length; i++){
+				outfile.WriteLine($"{xs_2[i]} {ys_2[i]}");
+			}
+
+			outfile.WriteLine("\n");
+
+			var (xx2, res2) = berrut.B1_interp(xs_2, ys_2, -1.01, 1.01, 1000);
+			for(int i=0; i<res2.Length; i++){
+				outfile.WriteLine($"{xx2[i]} {res2[i]}");
+			}
+
+
+			// Test 3 - interpolating the function
+			outfile.WriteLine("\n");
+			for(int i=0; i<xs_3.Length; i++){
+				outfile.WriteLine($"{xs_3[i]} {ys_3[i]}");
+			}
+
+			outfile.WriteLine("\n");
+
+			var (xx3, res3) = berrut.B1_interp(xs_3, ys_3, -1.01, 1.01, 1000);
+			for(int i=0; i<res1.Length; i++){
+				outfile.WriteLine($"{xx3[i]} {res3[i]}");
+			}
+
+			// Test 4 - interpolating the function
+			outfile.WriteLine("\n");
+			for(int i=0; i<xs_4.Length; i++){
+				outfile.WriteLine($"{xs_4[i]} {ys_4[i]}");
+			}
+
+			outfile.WriteLine("\n");
+
+			var (xx4, res4) = berrut.B1_interp(xs_4, ys_4, -1.01, 1.01, 1000);
+			for(int i=0; i<res4.Length; i++){
+				outfile.WriteLine($"{xx4[i]} {res4[i]}");
+			}
+			
+			/* 1st approach
 			// Test 1 - interpolating the function
 			for(int i=0; i<xs_1.Length; i++){
 				outfile.WriteLine($"{xs_1[i]} {ys_1[i]}");
@@ -86,7 +166,7 @@ public class main{
 			for(double z=-1.01; z<=1.01; z+=1.0/64){
 				double q = berrut.B1_interp(z, xs_4, ys_4);
 				outfile.WriteLine($"{z} {q}");
-			}
+			}*/
 		}
 
 		// Compare to the cspline interpolation algorithm
